a8091edd4be35ac2eadf4e37d07f661a0b533775
Junpyo <bluepeng9@gmail.com>
2022-05-30 04:48:00 +0900

    modify audio controller

--- lib/src/ui/5/note_read.dart
+++ lib/src/ui/5/note_read.dart
@@ -1,17 +1,10 @@
+import 'package:chewie_audio/chewie_audio.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:lupin_app/src/model/course_post_model.dart';
-import 'package:lupin_app/src/model/post_model.dart';
-import 'package:lupin_app/src/model/post_detail_model.dart';
 import 'package:lupin_app/src/model/note_model.dart';
-import 'package:lupin_app/src/model/comment_model.dart';
-import 'package:lupin_app/src/apis.dart';
-import 'package:dio/dio.dart';
-import 'package:lupin_app/src/provider/post_provider.dart';
-import 'package:lupin_app/src/ui/3/board.dart';
-import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
-import '../../provider/app_state_provider.dart';
+import 'package:video_player/video_player.dart';
 
 class NoteRead extends StatefulWidget {
   final Note note;
@@ -43,83 +36,121 @@ class _NoteReadState extends State<NoteRead> {
     return response.realUri;
   }
 
+  VideoPlayerController? videoPlayerController;
+
+  var chewieController;
+
+  var playerWidget;
+
+  @override
+  void dispose() {
+    videoPlayerController?.dispose();
+    chewieController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    Future.microtask(() async {
+      videoPlayerController = VideoPlayerController.network(uri.toString());
+      await videoPlayerController!.initialize();
+      chewieController = ChewieAudioController(
+        videoPlayerController: videoPlayerController!,
+        autoPlay: false,
+        looping: false,
+      );
+      setState(() {});
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    if(widget.note.type == 'SCRIPT'){
+    if (widget.note.type == 'SCRIPT') {
       _contentController.text = widget.note.script!;
-    } else if(widget.note.type == 'SUMMARY'){
+    } else if (widget.note.type == 'SUMMARY') {
       _contentController.text = widget.note.summary!;
-    } else if(widget.note.type == 'QUESTION'){
+    } else if (widget.note.type == 'QUESTION') {
       _contentController.text = widget.note.content!;
-    } else if(widget.note.type == 'RECORDING'){
+    } else if (widget.note.type == 'RECORDING') {
       uri = Uri.parse(widget.note.recordKey!);
-      _launchUrl();
+      // _launchUrl();
+
     }
+
     return Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            backgroundColor: Colors.white,
-            title: Text(
-              '강의노트',
-            ),
-          ),
-          body: SafeArea(
-            child: Column(
-              children: [
-                Container(
-                  child:
-                  ListView(
-                    shrinkWrap: true,
-                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                    children: <Widget>[
-                      SizedBox(height: 40.0),
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border(bottom: BorderSide(color: Colors.grey)),
-                        ),
-                        child: Row(
-                            children: [
-                              Expanded(
-                                flex: 2,
-                                child: Text(widget.note.type),
-                              ),
-                              Container(width: 10,),
-                              Expanded(
-                                flex: 1,
-                                child: Text(
-                                  widget.note.createdAt.substring(6,10).replaceAll('-', '/') + ' ' + widget.note.createdAt.substring(11,16),
-                                  textAlign: TextAlign.right,
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                  ),),
-                              ),
-                            ]
-                        ),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        title: Text(
+          '강의노트',
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              child: ListView(
+                shrinkWrap: true,
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                children: <Widget>[
+                  SizedBox(height: 40.0),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border(bottom: BorderSide(color: Colors.grey)),
+                    ),
+                    child: Row(children: [
+                      Expanded(
+                        flex: 2,
+                        child: Text(widget.note.type),
                       ),
-                      SizedBox(height: 10.0),
-                      TextFormField(
-                        controller: _contentController,
-                        readOnly: true,
-                        maxLines: 20,
-                        textInputAction: TextInputAction.next,
-                        decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey, width: 0.0),
-                            borderRadius: BorderRadius.all(
-                              Radius.elliptical(13, 13),
-                            ),
+                      Container(
+                        width: 10,
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Text(
+                          widget.note.createdAt
+                                  .substring(6, 10)
+                                  .replaceAll('-', '/') +
+                              ' ' +
+                              widget.note.createdAt.substring(11, 16),
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                            color: Colors.grey,
                           ),
                         ),
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
                       ),
-                    ],
+                    ]),
                   ),
-                ),
-              ],
+                  SizedBox(height: 10.0),
+                  if (chewieController != null)
+                    ChewieAudio(
+                      controller: chewieController,
+                    ),
+                  // TextFormField(
+                  //   controller: _contentController,
+                  //   readOnly: true,
+                  //   maxLines: 20,
+                  //   textInputAction: TextInputAction.next,
+                  //   decoration: InputDecoration(
+                  //     focusedBorder: OutlineInputBorder(
+                  //       borderSide: BorderSide(color: Colors.grey, width: 0.0),
+                  //       borderRadius: BorderRadius.all(
+                  //         Radius.elliptical(13, 13),
+                  //       ),
+                  //     ),
+                  //   ),
+                  //   style: TextStyle(
+                  //     fontSize: 18,
+                  //   ),
+                  // ),
+                ],
+              ),
             ),
-          ),
-        );
+          ],
+        ),
+      ),
+    );
   }
 }
